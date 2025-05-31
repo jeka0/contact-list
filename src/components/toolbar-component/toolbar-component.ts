175149bf0476b5e34471503e2db28faf372467c0
jeka2 <latosh02@mail.ru>
2025-05-31 12:41:04 +0300

    feat: added event creation by button click

--- src/components/toolbar-component/toolbar-component.ts
+++ src/components/toolbar-component/toolbar-component.ts
@@ -1,18 +1,59 @@
 import html from './toolbar-component.html?raw';
 import cssUrl from './toolbar-component.scss?url';
+import { Events } from '../../enum/events-enum';
 
 const template = document.createElement('template');
 template.innerHTML = `
-  <link rel="stylesheet" href="${cssUrl}">
-  ${html}
+    <link rel="stylesheet" href="${cssUrl}">
+    ${html}
 `;
 
 export class ToolbarComponent extends HTMLElement {
-  constructor() {
-    super();
-    const shadow = this.attachShadow({ mode: 'open' });
-    shadow.appendChild(template.content.cloneNode(true));
-  }
+    private shadowRootRef: ShadowRoot;
+    private groupsButton: HTMLButtonElement | null = null;
+    private addContactButton: HTMLButtonElement | null = null;
+
+    constructor() {
+        super();
+        this.shadowRootRef = this.attachShadow({ mode: 'open' });
+        this.shadowRootRef.appendChild(template.content.cloneNode(true));
+    }
+
+    connectedCallback() {
+        this.groupsButton = this.shadowRootRef.querySelector('.groups');
+        this.addContactButton = this.shadowRootRef.querySelector('.add');
+        this.addClickHandler(this.groupsButton, this.handleGroupsClick);
+        this.addClickHandler(this.addContactButton, this.handleAddContactClick);
+    }
+
+    disconnectedCallback() {
+        this.removeClickHandler(this.groupsButton, this.handleGroupsClick);
+        this.removeClickHandler(this.addContactButton, this.handleAddContactClick);
+    }
+
+    private handleGroupsClick() : void {
+        this.defaultClickHandler(Events.SHOW_GROUPS_SIDEBAR)
+    }
+
+    private handleAddContactClick() : void {
+        this.defaultClickHandler(Events.SHOW_CONTACT_FORM)
+    }
+
+    private defaultClickHandler(eventName: string) {
+        this.dispatchEvent(new CustomEvent(eventName, { bubbles: true, composed: true }));
+    }
+
+    private addClickHandler(button: HTMLButtonElement | null, handler: () => void) {
+        if (button) {
+            button.addEventListener('click', handler.bind(this));
+        }
+    }
+
+    private removeClickHandler(button: HTMLButtonElement | null, handler: () => void) {
+        if (button) {
+            button.removeEventListener('click', handler.bind(this));
+        }
+    }
 }
 
 customElements.define('toolbar-component', ToolbarComponent);
---
+++ src/enum/events-enum.ts
@@ -0,0 +1,4 @@
+export class Events {
+    static readonly SHOW_GROUPS_SIDEBAR = 'show-groups-sidebar';
+    static readonly SHOW_CONTACT_FORM = 'show-add-contact-form';
+};
