2bb0cfadad4822b6323f7eef18642a5658265278
jeka2 <latosh02@mail.ru>
2025-05-31 21:36:06 +0300

    feat: added connection of SidebarComponent to MainComponent

--- src/components/main-component/main-component.ts
+++ src/components/main-component/main-component.ts
@@ -1,18 +1,69 @@
 import html from './main-component.html?raw';
 import cssUrl from './main-component.scss?url';
+import { SidebarComponent } from '../sidebar-component/sidebar-component';
+import { GroupListComponent } from '../group-list-component/group-list-component';
+import { ContactFormComponent } from '../contact-form-component/contact-form-component';
+import { Events } from '../../enum/events-enum';
 
 const template = document.createElement('template');
 template.innerHTML = `
-  <link rel="stylesheet" href="${cssUrl}">
-  ${html}
+    <link rel="stylesheet" href="${cssUrl}">
+    ${html}
 `;
 
 export class MainComponent extends HTMLElement {
-  constructor() {
-    super();
-    const shadow = this.attachShadow({ mode: 'open' });
-    shadow.appendChild(template.content.cloneNode(true));
-  }
+    private shadowRootRef: ShadowRoot;
+    private sidebarComponent: SidebarComponent | null = null;
+
+    constructor() {
+        super();
+        this.shadowRootRef = this.attachShadow({ mode: 'open' });
+        this.shadowRootRef.appendChild(template.content.cloneNode(true));
+    }
+
+    connectedCallback() {
+        this.sidebarComponent = this.shadowRootRef.querySelector('sidebar-component');
+
+        document.addEventListener(Events.SHOW_GROUPS_SIDEBAR, this.handleShowGroupsSidebar.bind(this));
+        document.addEventListener(Events.SHOW_CONTACT_FORM, this.handleShowAddContactForm.bind(this));
+        document.addEventListener(Events.SIDEBAR_CLOSED, this.handleSidebarClosed.bind(this));
+        document.addEventListener(Events.FORM_SUBMITTED, this.handleSidebarClosed.bind(this));
+    }
+
+    disconnectedCallback() {
+        document.removeEventListener(Events.SHOW_GROUPS_SIDEBAR, this.handleShowGroupsSidebar.bind(this));
+        document.removeEventListener(Events.SHOW_CONTACT_FORM, this.handleShowAddContactForm.bind(this));
+        document.removeEventListener(Events.SIDEBAR_CLOSED, this.handleSidebarClosed.bind(this));
+        document.removeEventListener(Events.FORM_SUBMITTED, this.handleSidebarClosed.bind(this));
+    }
+
+    private handleShowGroupsSidebar() {
+        if (this.sidebarComponent) {
+            this.sidebarComponent.setAttribute('open', '');
+            const titleElementGroups = this.sidebarComponent?.shadowRoot?.querySelector('.sidebar-title');
+            if (titleElementGroups) {
+                titleElementGroups.textContent = 'Группы контактов';
+            }
+            this.sidebarComponent.setContent(new GroupListComponent());
+        }
+    }
+
+    private handleShowAddContactForm() {
+        if (this.sidebarComponent) {
+            this.sidebarComponent.setAttribute('open', '');
+            const titleElementAddContact = this.sidebarComponent?.shadowRoot?.querySelector('.sidebar-title');
+            if (titleElementAddContact) {
+                titleElementAddContact.textContent = 'Добавить контакт';
+            }
+            this.sidebarComponent.setContent(new ContactFormComponent(), { contact: null, mode: 'add' });
+        }
+    }
+
+    private handleSidebarClosed() {
+        if (this.sidebarComponent) {
+            this.sidebarComponent.removeAttribute('open');
+        }
+    }
 }
 
 customElements.define('main-component', MainComponent);
