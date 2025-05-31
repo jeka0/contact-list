9a721c17b7d7a493e0596a8bfecb0b5c6d5ea602
jeka2 <latosh02@mail.ru>
2025-05-31 13:07:50 +0300

    feat: added SidebarComponent

--- src/enum/events-enum.ts
+++ src/enum/events-enum.ts
@@ -1,4 +1,5 @@
 export class Events {
     static readonly SHOW_GROUPS_SIDEBAR = 'show-groups-sidebar';
     static readonly SHOW_CONTACT_FORM = 'show-add-contact-form';
+    static readonly SIDEBAR_CLOSED = 'sidebar-closed';
 };
